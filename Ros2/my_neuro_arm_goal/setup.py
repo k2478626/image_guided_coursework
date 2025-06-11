da16d9161af4cfeae9e0835fa2bd3ece68ed664d
awp <k2478626@kcl.ac.uk>
2025-06-11 23:38:36 +0100

    Update setup.py

--- Ros2/my_neuro_arm_goal/setup.py
+++ Ros2/my_neuro_arm_goal/setup.py
@@ -1,4 +1,3 @@
-import glob
 from setuptools import setup
 
 package_name = 'my_neuro_arm_goal'
@@ -8,18 +7,22 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob.glob('launch/*.py')),
+        ('share/' + package_name + '/launch', ['launch/my_neuro_arm_goal.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Andrea',
-    description='A custom ROS 2 Python package to plan motions for neuro arm using MoveItPy',
-    license='Apache 2.0',
+    maintainer='Andrea',        
+    maintainer_email='awalkerperez@gmail.com', 
+    description='My Neuro Arm Goal Python package',
+    license='TODO: License declaration', 
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'my_robot_goal = my_neuro_arm_goal.my_robot_goal:main',
+            'my_neuro_arm_goal = my_neuro_arm_goal.my_neuro_arm_goal_node:main', 
         ],
     },
 )
+
